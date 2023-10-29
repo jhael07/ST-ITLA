24d412932825cfc8129fa4968b28a7f8bae58830
jhael07 <jhaelr007@gmail.com>
2023-10-29 01:49:59 -0400

    more comments

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -6,10 +6,11 @@ import "./index.css";
 const Login = () => {
   return (
     <div class="bg-slate-50 flex justify-center items-center min-h-screen ">
-      {/* 👇 Este es el banner del itla con la frase al lado del login */}
+      {/* 👇 Este es el banner del itla con la frase */}
       <CampusBanner />
       <div class="lg:p-36 md:p-52 sm:20 p-8 pt-32 w-full lg:w-1/2 h-screen  ">
         <h1 class="text-2xl font-semibold mb-4">Login</h1>
+        {/* --- FORMULARIO ---  */}
         <form action="#" method="POST" className="z-40">
           {/* 👇 Estos son los inputs de usuario y contraseña */}
           <InputsLogin {...inputs} />
